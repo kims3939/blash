bd590c81b06b6840d5a59b8eeb4003ed02f63194
김민우 (IT BDO Group) <kim.minwoo@kt.com>
2022-04-26 23:14:25 +0900

    first commit

---
+++ src/main/java/com/trade/blash/BlashApplication.java
@@ -0,0 +1,13 @@
+package com.trade.blash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlashApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlashApplication.class, args);
+	}
+
+}
